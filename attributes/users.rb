03e75d0d015884034422aa1f1f28f8cd98cb44c9
Colin Hubert <chubert@turbine.com>
2014-09-16 17:38:14 -0400

    adding new attributes for handling mongos and mongod nodes

--- attributes/users.rb
+++ attributes/users.rb
@@ -6,10 +6,19 @@ default['mongodb']['authentication']['password'] = 'admin'
 default['mongodb']['admin'] = {
   'username' => default['mongodb']['authentication']['username'],
   'password' => default['mongodb']['authentication']['password'],
-  'roles' => %w(userAdminAnyDatabase dbAdminAnyDatabase),
+  'roles' => %w(userAdminAnyDatabase dbAdminAnyDatabase clusterAdmin),
   'database' => 'admin'
 }
 
 default['mongodb']['users'] = []
 
+# Force creation of admin user. auth=true is an invalid
+# setting for mongos so this is needed to ensure the admin
+# user is created
 default['mongodb']['mongos_create_admin'] = false
+
+# For mongod replicasets, the delay in seconds and number
+# of times to retry adding a user. Used to handle election
+# of primary not being completed immediately
+default['mongodb']['mongod_create_user']['retries'] = 2
+default['mongodb']['mongod_create_user']['delay'] = 10
