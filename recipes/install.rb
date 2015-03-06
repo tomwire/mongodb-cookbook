f2802fe90d87774b4cb964a666261e94bf3f682d
Mihir Singh <git.service@mihirsingh.com>
2015-03-06 16:25:36 -0600

    Renamed the packager_opts attribute to packager_options

--- recipes/install.rb
+++ recipes/install.rb
@@ -60,22 +60,9 @@ template init_file do
   end
 end
 
-case node['platform_family']
-when 'debian'
-  # this options lets us bypass complaint of pre-existing init file
-  # necessary until upstream fixes ENABLE_MONGOD/DB flag
-  packager_opts = '-o Dpkg::Options::="--force-confold" --force-yes'
-when 'rhel'
-  # Add --nogpgcheck option when package is signed
-  # see: https://jira.mongodb.org/browse/SERVER-8770
-  packager_opts = '--nogpgcheck'
-else
-  packager_opts = ''
-end
-
 # install
 package node[:mongodb][:package_name] do
-  options packager_opts
+  options node[:mongodb][:packager_options]
   action :install
   version node[:mongodb][:package_version]
 end
